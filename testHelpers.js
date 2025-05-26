7bd61a4cdb0a234983ff18e2a7a8f09be8f4148b
Matt Currier <fingerskier@gmail.com>
2025-05-26 10:29:27 -0500

    add docs & cleanup

--- testHelpers.js
+++
@@ -1,5 +0,0 @@
-const randomString = () => Math.random().toString(36).substring(2, 15)
-const randomInt = () => Math.floor(Math.random() * 1000000)
-const sleep = async (ms) => new Promise(resolve => setTimeout(resolve, ms))
-
-export {randomString, randomInt, sleep}
